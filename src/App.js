0b98011db0d6b719d70159b8e6083460c18c0862
WilmerCevallos <jawscevallos@hotmail.com>
2021-04-03 12:54:02 -0500

    crud full in firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { isEmpty, size } from 'lodash'
 import shortid from 'shortid'
-import { getCollection } from './actions'
+import { addDocument, deleteDocument, getCollection, updateDocument } from './actions'
 
 function App() {
 
@@ -14,7 +14,9 @@ function App() {
   useEffect(() => {
     (async () => {
       const result = await getCollection("tasks")
-      console.log(result)
+      if (result.statusResponse){
+        setTasks(result.data)
+      }
     })()
     }, [])
 
@@ -29,24 +31,32 @@ function App() {
     return isValid
   }
 
-  const addTask = (e) => {
+  const addTask = async (e) => {
     e.preventDefault()
     if (!validForm()){
       return
     }
 
-    const newTask = {
-      id: shortid.generate(),
-      name: task
+    const result = await addDocument("tasks", {name: task})
+    if (!result.statusResponse){
+      setError(result.error)
+      return
     }
 
-    setTasks([ ...tasks, newTask ])
+    setTasks([ ...tasks, { id: result.data.id, name: task } ])
     setTask("")
   }
 
-  const saveTask = (e) => {
+  const saveTask = async (e) => {
     e.preventDefault()
+
     if (!validForm()){
+      return
+    }
+
+    const result = await updateDocument("tasks", id, { name: task})
+    if (!result.statusResponse) {
+      setError(result.error)
       return
     }
 
@@ -57,7 +67,13 @@ function App() {
     setId("")
   }
 
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    const result = await deleteDocument("tasks", id)
+    if (!result.statusResponse) {
+      setError(result.error)
+      return
+    }
+    
     const filteredTasks = tasks.filter(task => task.id !== id)
     setTasks(filteredTasks)
   }
